f1f187ae9017a9c74ea90eba4dc14b95ad5bd66b
Danny Radosevich <deradosevich@gmail.com>
2024-02-13 11:34:24 -0700

    code examples

--- week03/examples.py
+++ week03/examples.py
@@ -1,12 +1,19 @@
 # Danny Radosevich
 # Example code for cosc1010
 
-#First the hello world basics 
+# First the hello world basics
+print("Hello World")
 
-#Then variable assignments, strings and numbers
+# Then variable assignments, strings and numbers
+var_1 = 13
+print(var_1)
+var_2 = "is an odd number"
+# Then string manipulations
+f_name = "ada"
+l_name = "lovelace"
+print(f"{f_name.upper()} {l_name.title()}")
 
-#Then string manipulations 
+# f-strings
+print(f"{var_1}\t{var_2}")
 
-#f-strings 
-
-#Mathematical operations
+# Mathematical operations
